d19a26f5b83d5e6fd5360f7ae4db4767b81a866c
kwrona-wsei <kwrona@wsei.edu.pl>
2025-11-07 22:59:15 +0100

    Add migration for adding comments

--- lab3/Migrations/20251107215851_AddComments.Designer.cs
+++ lab3/Migrations/20251107215851_AddComments.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using lab3;
 
@@ -10,9 +11,11 @@ using lab3;
 namespace lab3.Migrations
 {
     [DbContext(typeof(BlogContext))]
-    partial class BlogContextModelSnapshot : ModelSnapshot
+    [Migration("20251107215851_AddComments")]
+    partial class AddComments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
