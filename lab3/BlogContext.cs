0344af11b4b8ae2386e15f12e6909f54bd8b75fb
kwrona-wsei <kwrona@wsei.edu.pl>
2025-11-07 20:28:39 +0100

    Add blog context

---
+++ lab3/BlogContext.cs
@@ -0,0 +1,18 @@
+﻿namespace lab3
+{
+    using aspNetMVC.Models.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    namespace YourProjectName.Data
+    {
+        public class BlogContext : DbContext
+        {
+            public DbSet<BlogArticle> Articles { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+            {
+                options.UseSqlite("Data Source=blog.db");
+            }
+        }
+    }
+}
